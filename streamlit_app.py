9a2b4e0a12def4a17e06778cc77100a3acc38ac8
mteoKX <97864760+mteoKX@users.noreply.github.com>
2023-10-11 12:17:37 +0800

    Update streamlit_app.py
    remove raw json output
    normalize raw json output as dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,8 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
+# take a json version of the response and normalizes it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output the screen as a table
+streamlit.dataframe(fruityvice_normalized)
